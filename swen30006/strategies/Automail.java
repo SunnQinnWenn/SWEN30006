2b0a2860d5eab1da8fc2f338e1cdd10fd4a7e5b5
Unknown <qinwensun@outlook.com>
2018-09-18 15:57:12 +1000

    specify the Enum type as Simulation.RobotType

--- swen30006/strategies/Automail.java
+++ swen30006/strategies/Automail.java
@@ -13,7 +13,7 @@ public class Automail {
     private RobotFactory robotFactory;
 
 
-    public Automail(IMailPool mailPool, IMailDelivery delivery, final List<? extends Enum<?>> robotTypes) {
+    public Automail(IMailPool mailPool, IMailDelivery delivery, List<Simulation.RobotType> robotTypes) {
     	// Swap between simple provided strategies and your strategies here
     	    	
     	// Initialize the MailPool, robots array and robot factory
@@ -24,7 +24,7 @@ public class Automail {
 		this.robotFactory = new RobotFactory();
 
 		// Traverse the robot types, initialise robots accordingly and add them to the list
-    	for (Enum type : robotTypes){
+    	for (Simulation.RobotType type : robotTypes){
 			this.robots.add(robotFactory.createRobot(delivery, mailPool, (Simulation.RobotType) type));
 		}
 
